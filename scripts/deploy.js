e1e7d0cd1b4297e62ada13321ececa0c6063fb00
jedinrk <jedi.nrk@gmail.com>
2022-09-13 11:22:05 +0400

    Updated readme with contract address

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -8,20 +8,7 @@ const hre = require("hardhat");
 const web3 = require("web3");
 
 async function main() {
-  /*const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );*/
+  
   const [owner, otherAccount] = await ethers.getSigners();
 
   const WETH = await hre.ethers.getContractFactory("WETH");
